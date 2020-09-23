5c29307c33a81ff78a20de1ddfee94dbbc7b4773
kameshwar sah <56642875+kameshwar-web@users.noreply.github.com>
2020-09-23 01:56:45 -0400

    Angular Student Admission

--- ERP/src/app/app-routing.module.ts
+++ ERP/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { StudentadmissionComponent } from './studentadmission/studentadmission.component';
 
 const routes: Routes = [
   {
     path:'',component:HomeComponent
+  },
+
+  {
+    path:'studentadmission',component:StudentadmissionComponent
   }
   
 ]
